99d79ad4c166fb85aa4bd830a5a7331eb032b5f3
Felipe <felipe.acosta002@gmail.com>
2024-09-14 10:23:18 -0700

    added to ch05 dir, Concat.hs, Prettify.hs and modularity with PrettyJSON.hs

---
+++ ch05/Prettify.hs
@@ -0,0 +1,59 @@
+import Predule.<>
+
+data Doc = Empty
+         | Char Char
+         | Text String
+         | Line
+         | Concat Doc Doc
+         | Union Doc Doc
+           deriving (Show, Eq)
+
+empty :: Doc
+empty = Empty
+
+char :: Char -> Doc
+char c = Char c
+
+text :: String -> Doc
+text "" = Empty
+text s = Text s
+
+double :: Double -> Doc
+double d = text (show d)
+
+line :: Doc
+line = Line
+
+(<>) :: Doc -> Doc -> Doc
+Empty <> y = y
+x <> Empty = x
+x <> y = x `Concat` y
+
+punctuate :: Doc -> [Doc] -> [Doc]
+punctuate p [] = []
+punctuate p [d] = [d]
+punctuate p (d:ds) = (d <> p) : punctuate p ds
+
+hcat :: [Doc] -> Doc
+hcat = foldr (<>)
+
+fold :: (Doc -> Doc -> Doc) -> [Doc] -> Doc
+fold f = foldr f empty
+
+fsep :: [Doc[ -> Doc
+fsep = fold (</>)
+
+(</>) :: Doc -> Doc -> Doc
+x </> y = x <> softline <> y
+
+softline :: Doc 
+softline - group Line
+
+group :: Doc -> Doc
+group x = flatten x `Union` x
+
+flatten :: Doc -> Doc
+flatten (x `Concat` y) = flatten x `Concat` flatten y
+flatten Line = Char ' '
+flatten (x `Union` _) = flattenx
+flatten other = other
